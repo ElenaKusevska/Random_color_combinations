5a3aa5b7b75b3715cef766c6a05015c43649c679
ElenaKusevska <elena.kusevska@gmail.com>
2020-09-10 09:21:48 +0200

    start database

---
+++ Flask_website_sqlite_db/create_sqlite_db.py
@@ -0,0 +1,48 @@
+import sqlite3
+import datetime
+
+# Establish connection to local database:
+conn = sqlite3.connect('./db.sqlite') # Create database
+cur = conn.cursor()
+
+# Create table:
+cur.execute('DROP TABLE IF EXISTS color_combinations_rated')
+create_table = ( 'CREATE TABLE color_combinations_rated (' 
+        + 'id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, '
+        + 'time TIME NOT NULL,'
+        + 'date DATE NOT NULL,'
+        + 'color1 VARCHAR NOT NULL,'
+        + 'color2 VARCHAR NOT NULL,'
+        + 'rating INTEGER NOT NULL );' )
+cur.execute(create_table)
+
+# Create an entry to test the table and database are working:
+current_date_time = str(datetime.datetime.now()).split('.')[0]
+current_date = current_date_time.split(' ')[0]
+current_time = current_date_time.split(' ')[1]
+insert_test_value = ( 'INSERT INTO color_combinations_rated (time, date, color1, color2, rating) '
+     + 'VALUES ( "' + current_date + '", "' + current_time + '", "#ffffff", "#000000", 5);' )
+cur.execute(insert_test_value)
+
+# Create another entry to test the table and database are working:
+current_date_time = str(datetime.datetime.now()).split('.')[0]
+current_date = current_date_time.split(' ')[0]
+current_time = current_date_time.split(' ')[1]
+insert_test_value = ( 'INSERT INTO color_combinations_rated (time, date, color1, color2, rating) '
+     + 'VALUES ( "' + current_date + '", "' + current_time + '", "#ffffff", "#000000", 5);' )
+cur.execute(insert_test_value)
+
+# Print table:
+select_test = ( 'SELECT * from color_combinations_rated' )
+cur.execute(select_test)
+print(cur.fetchall())
+
+# Get table info:
+table_info = cur.execute("PRAGMA TABLE_INFO({})".format('color_combinations_rated'))
+table_info_tuples = [tup[:] for tup in table_info]
+print(table_info_tuples)
+print(" ")
+
+
+cur.close()
+conn.close()
--- README.md
+++ README.md
@@ -1,7 +1,16 @@
 # Random_color_combinations
 What makes people like a color combination?
 
-To do: Add field to rate random color combination
-from 1 to 10. Collect responses. Correlate
-score of color combination with characteristics of
-rgb_ values.
+This is a data science/web development project in Flask, using
+javascript, HTML, and CSS. When the web page is loaded, a combination
+of two colors is randomly selected and displayed. The user
+rates this combination of color, and the rating is stored
+in a database. The idea is that after a large number
+of people have given their ratings, a large enough database
+of color combinations, their respective ratings, and perhaps
+some information about the rater (location, etc.) will be compiled.
+Once enough data is available, we can correlate the characteristics of
+the relationships between the rgb values of the two colors with the
+rating of the color combinations, to discover what makes people find
+a color combination pleasing to look at.
+
